4c864a8b40ae2e7fb8d88b89bd771f8761d3fb45
xincan <xincan@mogu.com>
2021-07-30 17:14:16 +0800

    feat: 增加布局样式

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,14 +1,39 @@
 import { ProjectList } from "./pages/project-list";
 import { useAuthHook } from "./context/auth-context";
 import { Button } from "antd";
+import styled from "@emotion/styled";
+import { Row } from "./components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuthHook();
 
   return (
-    <div>
-      <Button onClick={logout}>登出</Button>
-      <ProjectList />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <Button onClick={logout}>登出</Button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectList />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+const Header = styled(Row)``;
+const Main = styled.main``;
+
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
